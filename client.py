523b0c57734faf53d9259455ad3c46139267e375
Alon Palgy <a.palgy@gmail.com>
2024-03-07 11:37:55 +0200

    lesson1 wip

--- client.py
+++ client.py
@@ -1,2 +1,22 @@
+from pprint import pprint
+from elasticsearch import Elasticsearch
+
+
 class ElasticClient:
-    pass
+    def __init__(self):
+        self.elastic_client = Elasticsearch("http://localhost:9200")
+        client_info = self.elastic_client.info()
+        pprint(client_info.body)
+
+    def create_index(self, index_name: str, mappings: dict):
+        self.elastic_client.indices.delete(index=index_name, ignore_unavailable=True)
+        self.elastic_client.indices.create(index=index_name, mappings=mappings)
+
+    def insert_document(self, index_name: str, document: dict):
+        return self.elastic_client.index(index=index_name, body=document)
+
+    def show_mappings(self, index_name: str):
+        return self.elastic_client.indices.get_mapping(index=index_name)
+
+    def retrieve_document(self, index_name: str, document_id: str):
+        return self.elastic_client.get(index=index_name, id=document_id)
--- main.py
+++ main.py
@@ -1,3 +1,39 @@
+from pprint import pprint
+
 from client import ElasticClient
 
 elastic_client = ElasticClient()
+
+my_name = "first_index"
+# res = elastic_client.show_mappings(index_name=my_name)
+# pprint(res)
+my_index_mappings = {
+    "dynamic": "strict",
+    "properties": {
+        "name": {"type": "text"},
+        "main_text": {
+            "type": "text",
+            "fields": {
+                "keyword": {
+                    "type": "keyword",
+                    "ignore_above": 256,
+                }
+            },
+        },
+    }
+}
+
+# elastic_client.create_index(index_name=my_name, mappings=my_index_mappings)
+
+my_doc = dict(
+    name="first name",
+    main_text="some random text"
+)
+# res = elastic_client.insert_document(index_name=my_name, document=my_doc)
+# pprint(res)
+
+my_doc_id = '5183GI4Bd2ZiWoz_4w5S'
+res_doc = elastic_client.retrieve_document(index_name=my_name, document_id=my_doc_id)
+pprint(res_doc)
+
+
